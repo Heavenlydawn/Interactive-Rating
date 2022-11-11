d86b108e1296188d5f28b0055283eafe5f357be2
Heavenlydawn <gabrielheavenlydawn@gmail.com>
2022-11-11 14:52:59 +0100

    Java Script Modification

--- script.js
+++ script.js
@@ -1,48 +1,22 @@
-// const btn = document.querySelectorAll(".btn");
-// const submit = document.querySelector(".submit");
-// const rateNum = document.getElementById("rate-num");
-
-// // const rateNum = document.querySelector('#rate-num')
-
-// // const rating = function () {
-// //   btn.forEach((i) => {
-// //     i.addEventListener("click", function () {
-// //       console.log(i);
-// //       rateNum.innerHTML = i.innerHTML;
-// //     });
-// //   });
-// // };
-
-
-// btn.forEach((i) => {
-//   i.addEventListener("click", function () {
-//     i.innerHTML = rateNum.innerHTML
-//     rateNum.innerHTML = i.innerHTML;
-//   });
-// });
-
-// submit.addEventListener("click", function () {
-//   location.href = "./rate.html";
-// });
-
 
 const btn = document.querySelectorAll(".btn");
 const submit = document.querySelector(".submit");
-const rateNum = document.getElementById("rate-num");
-
-let selectedValue = 0;
-
-btn.forEach((i) => {
-  i.addEventListener("click", function () {
-    selectedValue = i.innerHTML;
-    localStorage.setItem("value", selectedValue);
-  });
-});
-
+const rateNum = document.querySelector("#rate-num");
 
 submit.addEventListener("click", function () {
   location.href = "./rate.html";
 });
 
-rateNum.innerHTML = `you selected ${localStorage.getItem("value")} out of 5`;
-console.log(rateNum.innerHTML);
+let selectedValue;
+console.log(btn);
+
+btn.forEach((i) => {
+  i.addEventListener("click", function () {
+    selectedValue = i.value;
+    localStorage.setItem("btn", i.value);
+    console.log(selectedValue);
+  });
+});
+
+rateNum.textContent = localStorage.getItem("btn");
+console.log(rateNum.textContent);
